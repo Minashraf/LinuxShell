5cc7a56b75e52c60191066b07144ec52d70b036a
Mina Ashraf <minachraf@outlook.com>
2020-11-04 13:47:49 +0200

    History added and arrow keys move freely

--- main.c
+++ main.c
@@ -4,38 +4,13 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <time.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+
 #define MAX 1000
 
 FILE *fptr;
 char *parsed[MAX];
-
-void RemoveLeadingSpaces(char *str)
-{
-    char str1[MAX];
-    int count = 0,k,j;
-
-    while (str[count] == ' ' || str[count] == '\t')
-        ++count;
-
-    for (j = count, k = 0;str[j] != '\0'; j++, k++)
-        str1[k] = str[j];
-
-    str1[k-1] = '\0';
-    strcpy(str, str1);
-}
-
-void TrimTrailing(char *str)
-{
-    int index=-1,i=0;
-    while(str[i] != '\0')
-    {
-        if(str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
-            index= i;
-
-        i++;
-    }
-    str[index+1]='\0';
-}
 
 void logfile(int ErrorType)
 {
@@ -116,7 +91,7 @@ void execute(char *command)
             }
             exit(0);
         } else {
-            sleep((unsigned int) 0.5);
+            sleep(1);
             if (strcmp(parsed[index - 1], "&")!=0)
                 wait(NULL);
             else
@@ -135,16 +110,17 @@ int main() {
     while(1)
     {
         fflush(stdin);
-        printf("\n%s's Shell >>> ",getenv("USER"));
+        printf("\n%s",getenv("USER"));
         char command[MAX];
-        fgets(command, MAX, stdin);
-        RemoveLeadingSpaces(command);
-        TrimTrailing(command);
-        if(!strcmp("exit",command))
-        {   printf("Goodbye!");
-            fclose(fptr);
-            exit(0);
+        char *buf = readline("'s Shell >>> ");
+        if (strlen(buf) != 0) {
+            add_history(buf);
+            strcpy(command, buf);
+            if(!strcmp("exit",command))
+            {   fclose(fptr);
+                exit(0);
+            }
+            execute(command);
         }
-        execute(command);
     }
 }
